d8c5e90f8cc01b8adbc63b093ef43b61a907cf3a
chamamas <30767999+chamamas@users.noreply.github.com>
2018-02-12 09:33:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -18,51 +18,6 @@
     <option id="BMR_fun" value="BMR.php">Basal Metabolic Rate (BMR)</option>
     <option id="Cholesterol_fun" value="Cholesterol.php">Cholesterol</option>
 </select>
-<form id="BMI" action="<?php echo $_SERVER['PHP_SELF']; ?>;"  method="POST">
-<p>ความสูง:</p>
-<input type="text" name='height'>
-<br><br>
-<p>นํ้าหนัก:</p>
-<input type="text" name='weight'>
-<br><br>
-<input type="submit">
-</form>
-</center>
-<center>
-<p>
-<?php
-if($_SERVER['REQUEST_METHOD']=="POST"){
-echo "<br>";
-$a = $_POST['weight'] / ((($_POST['height'])/100)**2);
-echo "ดัชนีมวลกายของคุณ : " .$a;
-}?>
-</p>
-<form id="BMR" action="<?php echo $_SERVER['PHP_SELF']; ?>"  method="POST">
-<p>ความสูง:</p>
-<input type="text" name='height'>
-<br><br>
-<p>นํ้าหนัก:</p>
-<input type="text" name='weight'>
-<br><br>
-<p>อายุ:</p>
-<input type="text" name='age'>
-<br><br>
-<p>เพศ:</p>
-<p>
-<input type="radio" name="gender"  value="female">Female
-<input type="radio" name="gender"  value="male">Male
-</p>
-<br><br>
-<select name="exercise" class="myButton">
-    <option value="1.2">ไม่ออกกำลังกายหรือออกกำลังกายน้อยมาก</option>
-    <option value="1.375">ออกกำลังกายน้อยเล่นกีฬา 1-3 วัน/สัปดาห์</option>
-    <option value="1.55">ออกกำลังกายปานกลางเล่นกีฬา 3-5 วัน/สัปดาห์</option>
-    <option value="1.725">ออกกำลังกายหนักเล่นกีฬา 6-7 วัน/สัปดาห์</option>
-    <option value="1.9">ออกกำลังกายหนักมากเป็นนักกีฬา</option>
-</select>
-<br><br>
-<input type="submit">
-</form>
 <p>
 </body>
 </html>
